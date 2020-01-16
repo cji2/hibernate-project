5e96c4292549a0dbf46027ea37f5e6645eb0af52
Changhoon Ji <cji2@gmu.edu>
2020-01-16 06:51:32 -0500

    create main app. to get data from DB with HQL (Hibernate Query Language)

---
+++ hibernate-project/src/edu/gmu/hibernate/QueryStudentDB.java
@@ -0,0 +1,59 @@
+package edu.gmu.hibernate;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import edu.gmu.hibernate.entity.Student;
+
+public class QueryStudentDB {
+
+	public static void main(String[] args) {
+
+		// create session factory.
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// start transaction.
+			session.beginTransaction();
+			
+			// query students.
+			List<Student> Students = session
+									.createQuery("from Student")
+									.getResultList();
+			
+			// display the students.
+			displayStudents(Students);
+			
+			// query students: lastName='Ji'
+			Students = session
+					.createQuery("from Student s where s.lastName='Jeon'")
+					.getResultList();
+			
+			// display the students.
+			System.out.println("\nStudents who have the last name of 'Jeon': ");
+			displayStudents(Students);
+			
+			// commit the transaction.
+			session.getTransaction().commit();
+			System.out.println("Done!");
+		} 
+		finally {
+			factory.close();
+		}
+	}
+
+	private static void displayStudents(List<Student> Students) {
+		for (Student aStudent : Students) {
+			System.out.println(aStudent);
+		}
+	}
+}
